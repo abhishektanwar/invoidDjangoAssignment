1ae77fc5abc857bb2364cce4c645aa3581739e10
=abhishektanwar <abhishektanwar084@gmail.com>
2021-03-20 17:11:16 +0530

    readme update

--- invoidapp/encodeimage/views.py
+++ invoidapp/encodeimage/views.py
@@ -14,7 +14,6 @@ def returnCurrentTimestamp():
 	ct = datetime.datetime.now()
 	ts = ct.timestamp()
 	ts = bytes(str(ts),'utf-8')
-	# ts = str(ts)
 	return ts
 
 def encrypt_AES_GCM(msg, secretKey):
@@ -22,29 +21,34 @@ def encrypt_AES_GCM(msg, secretKey):
 	ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
 	return (ciphertext, aesCipher.nonce, authTag)
 
-# secretKey = os.urandom(32)  # 256-bit random encryption key
-# print("Encryption key:", binascii.hexlify(secretKey))
-# msg = returnCurrentTimestamp()
-# encryptedMsg = encrypt_AES_GCM(msg, secretKey)
-# print("encryptedMsg", {
-# 	'ciphertext': binascii.hexlify(encryptedMsg[0])
-# })
 
 @api_view(['GET','POST'])
 @csrf_exempt
 def imageEncode(request):
 	if request.method == 'POST':
 		dataFile = request.FILES['imageFile']
-		print(dataFile.name.split('.')[1])
+
+		# check if uploaded file has valid extension
+		file_extension = dataFile.name.split('.')[1]
+		print(file_extension)
+		allowed_file_types = ['jpeg','jpg','png']
+		if(file_extension not in allowed_file_types):
+			return JsonResponse(status = 400 , data = {"message":"file type not supported"})
+		
+		# base64 encryption
 		encoded_string = base64.b64encode(dataFile.read())
 		base64_str = encoded_string.decode('utf-8')
+
+		# md5 hash generation
 		hash_obj = hashlib.md5(encoded_string)
 		md5_hash = hash_obj.hexdigest()
+
+		# AES encryption of current timestamp
 		current_timestamp = returnCurrentTimestamp()
 		secretKey = os.urandom(32)  # 256-bit random encryption key
 		encryptedMsg = encrypt_AES_GCM(current_timestamp, secretKey)
 		AES_encrypted_timestamp = binascii.hexlify(encryptedMsg[0])
-		print("encryptedMsg",AES_encrypted_timestamp)
+				
 		return JsonResponse(status = 200, data = {"base64_str":base64_str,"md5":md5_hash,"timestamp":current_timestamp.decode('utf-8'),"AES_encrypted_timestamp":AES_encrypted_timestamp.decode('utf-8')})
 	
 	return render(request,'upload.html')
---
+++ invoidapp/readme.md
@@ -0,0 +1,15 @@
+# Django API for file image to base64 conversion.
+This API is hoted at https://invoiddjangoapp.herokuapp.com/encodeimage/
+
+
+ ## Installation 
+ ```sh
+$ cd invoidDjangoAssignment/
+$ virtualenv env_name
+$ source env_name/bin/activate
+$ cd invoidapp/
+$ pip3 install -r requirements.txt
+$ python3 manage.py runserver
+```
+
+#### locally hosted API can be accessed at http://localhost:8000/encodeimage/ 
