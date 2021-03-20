e2a1cb492354bee69fec2ca468842e30e0d082a9
=abhishektanwar <abhishektanwar084@gmail.com>
2021-03-20 12:46:32 +0530

    image encode functionality

--- invoidapp/encodeimage/views.py
+++ invoidapp/encodeimage/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import base64
 
 # Create your views here.
+@api_view(['GET','POST'])
+@csrf_exempt
+def imageEncode(request):
+	if request.method == 'POST':
+		dataFile = request.FILES['imageFile']
+		print(dataFile.name)
+		encoded_string = base64.b64encode(dataFile.read())
+		print(encoded_string.decode('utf-8'))
+		return JsonResponse({'info':'Django','name':"Abhishek","Str":encoded_string.decode('utf-8')})
+	
+	# return JsonResponse({'info':'Django React','name':"Abhishek"})
+	return render(request,'upload.html')
